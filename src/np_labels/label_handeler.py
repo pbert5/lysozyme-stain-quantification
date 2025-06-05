e90524e5effa4595b3b8eecd4715bd7b37ab125b
OpHPC-PHS <phsilbert@gmail.com>
2025-06-05 14:12:55 -0500

    created label_handeler skeleton

--- src/np_labels/label_handeler.py
+++ src/np_labels/label_handeler.py
@@ -1,1 +1,42 @@
+from __future__ import annotations
 
+class LabelHandeler:
+    def __init__(self, labels: list[int]):
+        """
+        Initialize the LabelHandeler with a list of labels.
+
+        Parameters:
+            labels (list[int]): A list of integer labels.
+        """
+        self.labels = labels
+    def merge_labels(self) -> LabelHandeler:
+        ... # Placeholder for the actual implementation of merging labels
+        return self # This method should contain the logic to merge labels based on specific criteria., should be able to chain together
+    def flood_fill(self, blobs, positive_mask) -> LabelHandeler:
+        """
+        Perform flood fill on the labels.
+
+        Returns:
+            LabelHandeler: The instance with flood-filled labels.
+        """
+        ...
+        return self
+    
+    class segmentation:
+        @staticmethod
+        def watershed_segmentation(image, **kwargs) -> LabelHandeler:
+            """
+            Perform watershed segmentation on the image.
+
+            Parameters:
+                image (np.ndarray): The input image for segmentation.
+                **kwargs: Additional keyword arguments for segmentation parameters.
+
+            Returns:
+                LabelHandeler: The instance with segmented labels.
+            """
+            ...
+            return LabelHandeler([])
+
+    
+    
